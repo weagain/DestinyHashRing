018619497667485dfba4341d3bb63d912bf07a0f
Weagain <chinaren.back@gmail.com>
2023-08-18 12:18:10 +0800

    fix value

--- migrations/2_migrate_hashring.js
+++ migrations/2_migrate_hashring.js
@@ -1,12 +1,22 @@
 const DestinyHashRing = artifacts.require("DestinyHashRing");
 
 var Web3 = require("web3")
-var provider = new Web3.providers.HttpProvider("https://rpc.etherfair.org")
-var web3 = new Web3(provider)
+var web3 = new Web3()
 
-let betValue = web3.utils.toWei('0.01', "ether");
 
-// deployed contract address: 0xdABaB46dc4a45786604D6b31D4a6ACA826B5C4F9
-module.exports = function (deployer) {
-  deployer.deploy(DestinyHashRing, betValue, '0xDC6F036a6FE27c8e70F4cf3b2f87Bd97a6b29a2f');
+module.exports = function (deployer, network) {
+  let bidValue = 0
+  
+  if(network=='ethf_mainnet') {
+    bidValue = web3.utils.toWei('1', 'ether');
+  } else if(network=='bsc_mainnet') {
+    bidValue = web3.utils.toWei('0.03', "ether");
+  }
+  if(bidValue==0) {
+    console.log('not set correct value')
+    return;
+  }
+
+  console.log('network===', network, bidValue)
+  deployer.deploy(DestinyHashRing, bidValue, '0x0000007915D5D3FF91aaFa880d1D9c352165B364');
 };
